946033be273a16f90c960ce204b1c0900eba3409
Dericay <anton.cj.sk8@gmail.com>
2023-11-10 13:48:21 +0100

    Add project files.

---
+++ Collection Generics/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collection_Generics
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public int Salary { get; set; }
+
+        public string Gender { get; set; }
+    }
+}
---
+++ Collection Generics/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace Collection_Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee em1 = new Employee()
+            {
+                ID = 111,
+                Name = "Tobias",
+                Salary = 25000,
+                Gender = "Male",
+            };
+
+            Employee em2 = new Employee()
+            {
+                ID = 112,
+                Name = "Anas",
+                Salary = 24000,
+                Gender = "Male",
+            };
+            Employee em3 = new Employee()
+            {
+                ID = 113,
+                Name = "Sara",
+                Salary = 24000,
+                Gender = "Female",
+            };
+            Employee em4 = new Employee()
+            {
+                ID = 114,
+                Name = "Eva",
+                Salary = 12000,
+                Gender = "Female",
+            };
+            Employee em5 = new Employee()
+            {
+                ID = 115,
+                Name = "Daniel",
+                Salary = 20000,
+                Gender = "Male",
+            };
+
+            Stack<Employee> EmpStack = new Stack<Employee>();
+
+            EmpStack.Push(em1);
+            EmpStack.Push(em2);
+            EmpStack.Push(em3);
+            EmpStack.Push(em4);
+            EmpStack.Push(em5);
+
+            
+            foreach (Employee emp in EmpStack)
+            {
+                Console.WriteLine("{0} - {1} - {2} - {3}", emp.ID, emp.Name, emp.Salary, emp.Gender);
+                Console.WriteLine("Item in stack = " + EmpStack.Count);
+                
+            }
+
+
+           
+                
+        }   
+    }
+}
